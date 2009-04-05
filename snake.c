6053275769ab5128f65f9a41d4cb4ae01f1623a6
Andreas Krennmair <ak@synflood.at>
2009-04-05 21:19:30 +0200

    removed obsolete license headers.

--- snake.c
+++ snake.c
@@ -1,19 +1,3 @@
-/* 
-   (c) 2000-2001 Andreas Krennmair 
-   SNAKE is free software; you can redistribute it and/or modify under the
-   terms of the GNU General Public License as published by the Free Software
-   Foundation; either version 2, or (at your opinion) any later version.
-
-   SNAKE is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-   details.
-
-   You should have received a copy of the GNU General Public License along with
-   SNAKE; see the file COPYING. If not, write to the Free Software Foundation,
-   675 Mass Ave, Cambridge, MA 02139, USA. 
-*/
-
 #include <stdlib.h>
 #include <ncurses.h>
 #include <string.h>
